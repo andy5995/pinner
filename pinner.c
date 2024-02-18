1b32737011dc3cfa07bce8ca9768e6c8e6af3e85
andy5995 <arch_stanton5995@protonmail.com>
2024-02-18 07:58:39 -0600

    reformat using Mozilla style

--- pinner.c
+++ pinner.c
@@ -6,78 +6,91 @@
  * https://github.com/andy5995/pinner
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
  */
 
 #include <geanyplugin.h>
 
-enum {
+enum
+{
   DO_PIN,
   DO_UNPIN
 };
 
-static void destroy_widget(gpointer pdata);
-static void clear_pinned_documents(void);
-static GtkWidget *create_popup_menu(const gchar *file_name);
-static gboolean on_button_press_cb(GtkWidget *widget, GdkEventButton *event, gpointer data);
-static gboolean is_duplicate(const gchar* file_name);
-static void pin_activate_cb(GtkMenuItem *menuitem, gpointer pdata);
-static void unpin_activate_cb(GtkMenuItem *menuitem, gpointer pdata);
+static void
+destroy_widget(gpointer pdata);
+static void
+clear_pinned_documents(void);
+static GtkWidget*
+create_popup_menu(const gchar* file_name);
+static gboolean
+on_button_press_cb(GtkWidget* widget, GdkEventButton* event, gpointer data);
+static gboolean
+is_duplicate(const gchar* file_name);
+static void
+pin_activate_cb(GtkMenuItem* menuitem, gpointer pdata);
+static void
+unpin_activate_cb(GtkMenuItem* menuitem, gpointer pdata);
 
-static GtkWidget *pinned_view_vbox;
+static GtkWidget* pinned_view_vbox;
 static gint page_number = 0;
-static GHashTable *doc_to_widget_map = NULL;
+static GHashTable* doc_to_widget_map = NULL;
 
-static void destroy_widget(gpointer pdata)
+static void
+destroy_widget(gpointer pdata)
 {
-  GtkWidget *widget = (GtkWidget *)pdata;
+  GtkWidget* widget = (GtkWidget*)pdata;
   gtk_widget_destroy(widget);
 }
 
-
-void clear_pinned_documents(void)
+void
+clear_pinned_documents(void)
 {
-  if (doc_to_widget_map != NULL)
-  {
+  if (doc_to_widget_map != NULL) {
     // Removes all keys and their associated values from the hash table.
-    // This will also call the destroy functions specified for keys and values,
-    // thus freeing the memory for the file names and destroying the widgets.
+    // This will also call the destroy functions specified for keys and
+    // values, thus freeing the memory for the file names and destroying the
+    // widgets.
     g_hash_table_remove_all(doc_to_widget_map);
   }
 }
 
-
-static GtkWidget *create_popup_menu(const gchar *file_name) {
-  GtkWidget *menu = gtk_menu_new();
+static GtkWidget*
+create_popup_menu(const gchar* file_name)
+{
+  GtkWidget* menu = gtk_menu_new();
 
   // Create a menu item without a label
-  GtkWidget *clear_item = gtk_menu_item_new();
+  GtkWidget* clear_item = gtk_menu_item_new();
   gtk_menu_shell_append(GTK_MENU_SHELL(menu), clear_item);
-  g_signal_connect_swapped(clear_item, "activate", G_CALLBACK(clear_pinned_documents), NULL);
+  g_signal_connect_swapped(
+    clear_item, "activate", G_CALLBACK(clear_pinned_documents), NULL);
 
   // Create a box to contain the icon and label
-  GtkWidget *hbox = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6); // 6 pixels spacing
+  GtkWidget* hbox =
+    gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6); // 6 pixels spacing
 
   // Create the icon
-  GtkWidget *clear_icon = gtk_image_new_from_icon_name("edit-clear", GTK_ICON_SIZE_MENU);
+  GtkWidget* clear_icon =
+    gtk_image_new_from_icon_name("edit-clear", GTK_ICON_SIZE_MENU);
 
   // Create the label
-  GtkWidget *label = gtk_label_new("Clear List");
+  GtkWidget* label = gtk_label_new("Clear List");
 
   // Pack the icon and label into the box
   gtk_box_pack_start(GTK_BOX(hbox), clear_icon, FALSE, FALSE, 0);
@@ -89,21 +102,27 @@ static GtkWidget *create_popup_menu(const gchar *file_name) {
   // Show all widgets
   gtk_widget_show_all(clear_item);
 
-  if (file_name != NULL)
-  {
+  if (file_name != NULL) {
     // Create a menu item without a label for unpinning a document
-    GtkWidget *unpin_item = gtk_menu_item_new();
+    GtkWidget* unpin_item = gtk_menu_item_new();
     gtk_menu_shell_append(GTK_MENU_SHELL(menu), unpin_item);
-    g_signal_connect_data(unpin_item, "activate", G_CALLBACK(unpin_activate_cb), g_strdup(file_name), (GClosureNotify)g_free, 0);
+    g_signal_connect_data(unpin_item,
+                          "activate",
+                          G_CALLBACK(unpin_activate_cb),
+                          g_strdup(file_name),
+                          (GClosureNotify)g_free,
+                          0);
 
     // Create a horizontal box to hold the icon and label
-    GtkWidget *hbox_unpin = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6); // 6 pixels spacing
+    GtkWidget* hbox_unpin =
+      gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 6); // 6 pixels spacing
 
     // Create the icon
-    GtkWidget *unpin_icon = gtk_image_new_from_icon_name("list-remove", GTK_ICON_SIZE_MENU);
+    GtkWidget* unpin_icon =
+      gtk_image_new_from_icon_name("list-remove", GTK_ICON_SIZE_MENU);
 
     // Create the label
-    GtkWidget *label_unpin = gtk_label_new("Unpin Document");
+    GtkWidget* label_unpin = gtk_label_new("Unpin Document");
 
     // Pack the icon and label into the horizontal box
     gtk_box_pack_start(GTK_BOX(hbox_unpin), unpin_icon, FALSE, FALSE, 0);
@@ -117,37 +136,35 @@ static GtkWidget *create_popup_menu(const gchar *file_name) {
   return menu;
 }
 
-
-static gboolean is_duplicate(const gchar* file_name)
+static gboolean
+is_duplicate(const gchar* file_name)
 {
   return g_hash_table_contains(doc_to_widget_map, file_name);
 }
 
-
-static void pin_document_key_cb(guint key_id)
+static void
+pin_document_key_cb(guint key_id)
 {
   pin_activate_cb(NULL, NULL);
 }
 
-static void unpin_document_key_cb(guint key_id)
+static void
+unpin_document_key_cb(guint key_id)
 {
   unpin_activate_cb(NULL, NULL);
 }
 
-
-static void pin_activate_cb(GtkMenuItem *menuitem, gpointer pdata)
+static void
+pin_activate_cb(GtkMenuItem* menuitem, gpointer pdata)
 {
-  gchar *ptr_file_name = NULL;
-  if (pdata == NULL)
-  {
-    GeanyDocument *doc = document_get_current();
+  gchar* ptr_file_name = NULL;
+  if (pdata == NULL) {
+    GeanyDocument* doc = document_get_current();
     if (doc == NULL)
       return;
     else
       ptr_file_name = doc->file_name;
-  }
-  else
-  {
+  } else {
     ptr_file_name = pdata;
   }
 
@@ -155,143 +172,157 @@ static void pin_activate_cb(GtkMenuItem *menuitem, gpointer pdata)
     return;
 
   /* This must be freed when nodes are removed from the list */
-  gchar *tmp_file_name = g_strdup(ptr_file_name);
+  gchar* tmp_file_name = g_strdup(ptr_file_name);
 
-  GtkWidget *event_box = gtk_event_box_new();
+  GtkWidget* event_box = gtk_event_box_new();
   g_hash_table_insert(doc_to_widget_map, tmp_file_name, event_box);
 
-  GtkWidget *label = gtk_label_new(ptr_file_name);
+  GtkWidget* label = gtk_label_new(ptr_file_name);
   // Enable ellipsizing at the start of the filename
   gtk_label_set_ellipsize(GTK_LABEL(label), PANGO_ELLIPSIZE_START);
   gtk_label_set_max_width_chars(GTK_LABEL(label), 30);
   // Set the label's alignment to left
   gtk_label_set_xalign(GTK_LABEL(label), 0.0);
   // Set margins
-  gtk_widget_set_margin_start(label, 10);  // 20 pixels margin on the start (left)
-  gtk_widget_set_margin_end(label, 10);    // 20 pixels margin on the end (right)
+  gtk_widget_set_margin_start(label,
+                              10);      // 20 pixels margin on the start (left)
+  gtk_widget_set_margin_end(label, 10); // 20 pixels margin on the end (right)
 
   gtk_container_add(GTK_CONTAINER(event_box), label);
   gtk_widget_show_all(event_box);
   gtk_box_pack_start(GTK_BOX(pinned_view_vbox), event_box, FALSE, FALSE, 0);
-  // gtk_notebook_set_current_page(GTK_NOTEBOOK(plugin->geany_data->main_widgets->sidebar_notebook), page_number);
+  // gtk_notebook_set_current_page(GTK_NOTEBOOK(plugin->geany_data->main_widgets->sidebar_notebook),
+  // page_number);
 
-  g_signal_connect(event_box, "button-press-event",
-    G_CALLBACK(on_button_press_cb), tmp_file_name);
+  g_signal_connect(event_box,
+                   "button-press-event",
+                   G_CALLBACK(on_button_press_cb),
+                   tmp_file_name);
 
   return;
 }
 
-
-static void unpin_activate_cb(GtkMenuItem *menuitem, gpointer pdata)
+static void
+unpin_activate_cb(GtkMenuItem* menuitem, gpointer pdata)
 {
-  GeanyDocument *doc = document_get_current();
+  GeanyDocument* doc = document_get_current();
   if (doc == NULL)
     return;
 
   gboolean removed = g_hash_table_remove(doc_to_widget_map, doc->file_name);
   // If removed
-  if (!removed)
-  {
+  if (!removed) {
     // Handle the case where the document was not found in the map
   }
 
   return;
 }
 
-
-static gboolean on_button_press_cb(GtkWidget *widget, GdkEventButton *event, gpointer data)
+static gboolean
+on_button_press_cb(GtkWidget* widget, GdkEventButton* event, gpointer data)
 {
-  if (event->type == GDK_BUTTON_PRESS && event->button == GDK_BUTTON_PRIMARY)
-  {
+  if (event->type == GDK_BUTTON_PRESS && event->button == GDK_BUTTON_PRIMARY) {
     // Check if the clicked widget is an event box
-    if (GTK_IS_EVENT_BOX(widget))
-    {
-      GtkWidget *label = gtk_bin_get_child(GTK_BIN(widget));
+    if (GTK_IS_EVENT_BOX(widget)) {
+      GtkWidget* label = gtk_bin_get_child(GTK_BIN(widget));
 
-      if (GTK_IS_LABEL(label))
-      {
-        const gchar *file_name = gtk_label_get_text(GTK_LABEL(label));
+      if (GTK_IS_LABEL(label)) {
+        const gchar* file_name = gtk_label_get_text(GTK_LABEL(label));
         document_open_file(file_name, FALSE, NULL, NULL);
       }
     }
-  }
-  else if (event->type == GDK_BUTTON_PRESS && event->button == GDK_BUTTON_SECONDARY)
-  {
+  } else if (event->type == GDK_BUTTON_PRESS &&
+             event->button == GDK_BUTTON_SECONDARY) {
     // Check if the clicked widget is an event box
-    if (GTK_IS_EVENT_BOX(widget))
-    {
-      GtkWidget *label = gtk_bin_get_child(GTK_BIN(widget));
+    if (GTK_IS_EVENT_BOX(widget)) {
+      GtkWidget* label = gtk_bin_get_child(GTK_BIN(widget));
 
-      if (GTK_IS_LABEL(label))
-      {
-        const gchar *file_name = gtk_label_get_text(GTK_LABEL(label));
-        GtkWidget *menu = create_popup_menu(file_name);
-        gtk_menu_popup_at_pointer(GTK_MENU(menu), (const GdkEvent *)event);
+      if (GTK_IS_LABEL(label)) {
+        const gchar* file_name = gtk_label_get_text(GTK_LABEL(label));
+        GtkWidget* menu = create_popup_menu(file_name);
+        gtk_menu_popup_at_pointer(GTK_MENU(menu), (const GdkEvent*)event);
         return TRUE;
       }
-    }
-    else
-    {
-      GtkWidget *menu = create_popup_menu(NULL);
-      gtk_menu_popup_at_pointer(GTK_MENU(menu), (const GdkEvent *)event);
+    } else {
+      GtkWidget* menu = create_popup_menu(NULL);
+      gtk_menu_popup_at_pointer(GTK_MENU(menu), (const GdkEvent*)event);
       return TRUE;
     }
   }
   return FALSE;
 }
 
-
-static gboolean pin_init(GeanyPlugin *plugin, gpointer pdata)
+static gboolean
+pin_init(GeanyPlugin* plugin, gpointer pdata)
 {
-  GtkWidget **tools_item = g_new0(GtkWidget*, 3); // Allocate memory for 3 pointers (2 items + NULL terminator)
+  GtkWidget** tools_item =
+    g_new0(GtkWidget*,
+           3); // Allocate memory for 3 pointers (2 items + NULL terminator)
   tools_item[DO_PIN] = gtk_menu_item_new_with_mnemonic("Pin Document");
   tools_item[DO_UNPIN] = gtk_menu_item_new_with_mnemonic("Unpin Document");
   tools_item[2] = NULL; // NULL sentinel
 
-  doc_to_widget_map = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, destroy_widget);
+  doc_to_widget_map =
+    g_hash_table_new_full(g_str_hash, g_str_equal, g_free, destroy_widget);
 
   gtk_widget_show(tools_item[DO_PIN]);
   gtk_container_add(GTK_CONTAINER(plugin->geany_data->main_widgets->tools_menu),
-    tools_item[DO_PIN]);
-  g_signal_connect(tools_item[DO_PIN], "activate",
-    G_CALLBACK(pin_activate_cb), NULL);
+                    tools_item[DO_PIN]);
+  g_signal_connect(
+    tools_item[DO_PIN], "activate", G_CALLBACK(pin_activate_cb), NULL);
 
   gtk_widget_show(tools_item[DO_UNPIN]);
   gtk_container_add(GTK_CONTAINER(plugin->geany_data->main_widgets->tools_menu),
-    tools_item[DO_UNPIN]);
-  g_signal_connect(tools_item[DO_UNPIN], "activate",
-    G_CALLBACK(unpin_activate_cb), NULL);
+                    tools_item[DO_UNPIN]);
+  g_signal_connect(
+    tools_item[DO_UNPIN], "activate", G_CALLBACK(unpin_activate_cb), NULL);
 
-  //g_signal_connect(event_box, "button-press-event",
-    //G_CALLBACK(on_button_press), NULL);
-  //g_signal_connect(pinned_view_vbox, "button-press-event",
-    //G_CALLBACK(on_button_press), NULL);
+  // g_signal_connect(event_box, "button-press-event",
+  // G_CALLBACK(on_button_press), NULL);
+  // g_signal_connect(pinned_view_vbox, "button-press-event",
+  // G_CALLBACK(on_button_press), NULL);
 
   geany_plugin_set_data(plugin, tools_item, NULL);
 
   pinned_view_vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 0);
   gtk_widget_show_all(pinned_view_vbox);
-  page_number = gtk_notebook_append_page(GTK_NOTEBOOK(plugin->geany_data->main_widgets->sidebar_notebook),
-    pinned_view_vbox, gtk_label_new(_("Pinned")));
+  page_number = gtk_notebook_append_page(
+    GTK_NOTEBOOK(plugin->geany_data->main_widgets->sidebar_notebook),
+    pinned_view_vbox,
+    gtk_label_new(_("Pinned")));
 
   // Keybinding setup
-  GeanyKeyGroup *key_group = plugin_set_key_group(plugin, "pinner_keys", 2, NULL);
-  keybindings_set_item(key_group, DO_PIN, pin_document_key_cb, 0, 0, "pin_document", "Pin Document", NULL);
-  keybindings_set_item(key_group, DO_UNPIN, unpin_document_key_cb, 0, 0, "unpin_document", "Unpin Document", NULL);
+  GeanyKeyGroup* key_group =
+    plugin_set_key_group(plugin, "pinner_keys", 2, NULL);
+  keybindings_set_item(key_group,
+                       DO_PIN,
+                       pin_document_key_cb,
+                       0,
+                       0,
+                       "pin_document",
+                       "Pin Document",
+                       NULL);
+  keybindings_set_item(key_group,
+                       DO_UNPIN,
+                       unpin_document_key_cb,
+                       0,
+                       0,
+                       "unpin_document",
+                       "Unpin Document",
+                       NULL);
 
   return TRUE;
 }
 
-
-static void pin_cleanup(GeanyPlugin *plugin, gpointer pdata)
+static void
+pin_cleanup(GeanyPlugin* plugin, gpointer pdata)
 {
-  if (doc_to_widget_map != NULL)
-  {
+  if (doc_to_widget_map != NULL) {
     g_hash_table_destroy(doc_to_widget_map);
     doc_to_widget_map = NULL;
   }
 
-  GtkWidget **tools_item = pdata;
+  GtkWidget** tools_item = pdata;
   while (*tools_item != NULL) {
     gtk_widget_destroy(*tools_item);
     tools_item++;
@@ -299,9 +330,9 @@ static void pin_cleanup(GeanyPlugin *plugin, gpointer pdata)
   g_free(pdata);
 }
 
-
 G_MODULE_EXPORT
-void geany_load_module(GeanyPlugin *plugin)
+void
+geany_load_module(GeanyPlugin* plugin)
 {
   plugin->info->name = "Pinner";
   plugin->info->description = "Pin a document";
